d3124a71bf4dd7c3de6ca84a876cb6335f0437f6
ValeriaLeonH <valerialeon837@gmail.com>
2023-11-01 17:11:43 -0400

    Save changes

--- estudio.cpp
+++ estudio.cpp
@@ -20,6 +20,35 @@ void insertar(lista **p, int n){
   }
 };
 
+void mostrarlista(lista *p, char c){
+  lista *ax = p; 
+  cout << "\n\n Contenido de la lista \n\n";
+  while (ax!=NULL){
+    cout << "[" << ax -> valor <<"]";
+    ax = ax -> prx; 
+  };
+};
+
+int buscar(lista *p, int n){
+  lista *ax=p; 
+  while (ax!=NULL){
+    if (ax -> valor == n){
+      ax = ax -> prx; 
+    };
+  };
+  return 0; 
+};
+
+void eliminar(lista **p, int x){
+  lista *ax = *p, *t;
+  if (ax){
+    if( ax -> valor == x){
+      *p = ax -> prx;
+      delete ax; 
+    };
+  };
+};
+
 int main(){
   int n=0, op=-1, res;
   lista *p=NULL;
@@ -30,8 +59,39 @@ int main(){
     cout << "3. Buscar un elemento\n";
     cout << "4. Eliminar un elemento \n";
     cout << "5. Cuantas veces esta un elemento  \n";
-    cout << "1. Insertar por cabeza \n";
+    cout << "6. Salir \n";
 
-  }
+    cin >> op;
+    switch (op){
+      case 1: 
+        cout<< "Indique un numero"; cin>>n;
+        insertar(&p, n);
+        cout<< "\n\n";
+      break;
+
+      case 2:
+        mostrarlista(p, 'P');
+      break;
+
+      case 3:
+        cout << "Introduzca el numero a buscar"; cin>> n;
+        res=buscar(p, n);
+        cout<< "El valor "<<n<<" ";
+        if (!(res)) cout<< "NO";
+          cout<< "No esta en la lista\n\n";
+      break;
+
+      case 4:
+        cout<< "Indique el numero que quiere eliminar"; cin>> n ;
+        eliminar(p,n);
+      break;
+
+      case 5: 
+
+      break;
+    };
+
+
+  };
   
 }
